2babf1db336f9f7ed2006e72f0e005e9577bf67c
marcuslannister <marcus@lannister.cc>
2023-11-01 18:16:00 +0800

    feat: Update org-agenda-files to include all .org files recursively

--- config.el
+++ config.el
@@ -49,6 +49,7 @@
   (setq org-agenda-start-day "-1d")
   (setq org-agenda-span 7)
   (setq org-agenda-start-on-weekday 1)
+  (setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
   (setq org-agenda-clockreport-parameter-plist
       (quote (:maxlevel 5 :fileskip0 t :compact t :narrow 80 :formula % ))))
 
