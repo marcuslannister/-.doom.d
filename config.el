efef088f7de86abfc8f26dfa9e1067f474a6c6da
marcuslannister <marcus@lannister.cc>
2023-11-03 16:12:56 +0800

    feat: Filter out ".stversions" directory from org-agenda-files

--- config.el
+++ config.el
@@ -49,7 +49,10 @@
   (setq org-agenda-start-day "-1d")
   (setq org-agenda-span 7)
   (setq org-agenda-start-on-weekday 1)
-  (setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
+  (setq org-agenda-files
+      (seq-filter (lambda(x) (not (string-match "/.stversions/"(file-name-directory x))))
+       (directory-files-recursively "~/org/" "\\.org$")
+       ))
   (setq org-agenda-clockreport-parameter-plist
       (quote (:maxlevel 5 :fileskip0 t :compact t :narrow 80 :formula % ))))
 
