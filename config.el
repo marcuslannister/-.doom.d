6bc7e9b097515e5370b4177a25d98224811499a8
marcuslannister <marcus@lannister.cc>
2024-04-13 18:44:42 +0800

    feat: add font weight

--- config.el
+++ config.el
@@ -28,8 +28,8 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :family "MonoLisa" :size 14 )
-      doom-variable-pitch-font (font-spec :family "MonoLisa" :size 14))
+(setq doom-font (font-spec :family "MonoLisa Nerd Font" :size 14 :weight 'medium)
+      doom-variable-pitch-font (font-spec :family "MonoLisa Nerd Font" :size 14 :weight 'medium))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
